83f0cb6fcae8d28f7d6eae16b3cbefabd7073676
Yabets Maregn <yabetsanon@gmail.com>
2025-07-18 18:19:08 +0600

    Add navigation to Visit page

---
+++ src/app/visit/page.tsx
@@ -0,0 +1,14 @@
+
+import Navigation from "../Navigation";
+
+export default function Visit() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <h1>Visit Us</h1>
+        <p>Find out how to visit our church and what to expect.</p>
+      </main>
+    </>
+  );
+}
