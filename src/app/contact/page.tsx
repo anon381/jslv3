9eee1d3554084b20d9d106aeaff8d4b62aac0431
Yabets Maregn <yabetsanon@gmail.com>
2025-07-18 18:19:08 +0600

    Add navigation to Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+
+import Navigation from "../Navigation";
+
+export default function Contact() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <h1>Contact Us</h1>
+        <p>Get in touch with us for more information.</p>
+      </main>
+    </>
+  );
+}
