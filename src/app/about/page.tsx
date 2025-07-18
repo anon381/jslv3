77a6b900d4ec4c4e19a71c1d0268eca98d9726e8
Yabets Maregn <yabetsanon@gmail.com>
2025-07-18 18:19:07 +0600

    Add navigation to About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+import Navigation from "../Navigation";
+
+export default function About() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <h1>About Us</h1>
+        <p>Learn more about our mission and values.</p>
+      </main>
+    </>
+  );
+}
