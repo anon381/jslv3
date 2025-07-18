4c7c3e6ec9b75744a697f38c73de95c8c965786c
Yabets Maregn <yabetsanon@gmail.com>
2025-07-18 18:19:07 +0600

    Add navigation to Sermons page

---
+++ src/app/sermons/page.tsx
@@ -0,0 +1,14 @@
+
+import Navigation from "../Navigation";
+
+export default function Sermons() {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <h1>Sermons</h1>
+        <p>Explore our latest sermons and teachings.</p>
+      </main>
+    </>
+  );
+}
