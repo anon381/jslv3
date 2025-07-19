4d1534203682533d3dded6ea3e967807516e940d
Yabets Maregn <yabetsanon@gmail.com>
2025-07-19 17:38:40 +0600

    Update About page background and text color

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <>
       <Navigation />
-      <main>
+      <main className="bg-white text-black min-h-screen p-8">
         <h1>About Us</h1>
         <p>Learn more about our mission and values.</p>
       </main>
