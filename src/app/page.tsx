94e4aa9ef3648133c632e3455744b238f55f927d
Yabets Maregn <yabetsanon@gmail.com>
2025-07-19 17:38:39 +0600

    Update Home page background and text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Navigation />
-      <main>
+      <main className="bg-white text-black min-h-screen p-8">
         <h1>Welcome to the Home Page</h1>
         <p>This is the main landing page of the site.</p>
       </main>
